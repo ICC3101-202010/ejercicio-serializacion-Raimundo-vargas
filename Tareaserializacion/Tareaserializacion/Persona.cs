3e6eb557eaac7023aec2ec4a6406554bbd868842
Raimundo Vargas Alcalde <rjvargas@miuandes.cl>
2020-05-07 10:29:51 -0400

    F

---
+++ Tareaserializacion/Tareaserializacion/Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Tareaserializacion
+{
+    [Serializable]
+    class Persona: ISerializable
+    {
+        public string Nombre;
+        public string Apellido;
+        public string Edad;
+        
+        public Persona(string nombre,string apellido, string edad)
+            {
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+            }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Nombre",Nombre);
+            info.AddValue("Apellido", Apellido);
+            info.AddValue("Edad", Edad);
+
+        }
+        public Persona(SerializationInfo info, StreamingContext context)
+        {
+            Nombre = (string)info.GetValue("Nombre", typeof(string));
+            Apellido = (string)info.GetValue("Apellido", typeof(string));
+            Edad = (string)info.GetValue("Edad", typeof(string));
+        }
+    }
+}
---
+++ Tareaserializacion/Tareaserializacion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Xml.Serialization;
+
+
+
+namespace Tareaserializacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("NOMBRE:\n");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("APELLIDO:\n");
+            string apellido= Console.ReadLine();
+            Console.WriteLine("EDAD:\n");
+            string edad = Console.ReadLine();
+            Persona persona = new Persona(nombre,apellido,edad);
+
+            Stream stream = File.Open("PersonaData.dat", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, persona);
+            stream.Close();
+            persona = null;
+
+            stream = File.Open("PersonaData.dat", FileMode.Open);
+            bf = new BinaryFormatter();
+            persona = (Persona)bf.Deserialize(stream);
+            stream.Close();
+            //Console.WriteLine(persona.ToString());
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
